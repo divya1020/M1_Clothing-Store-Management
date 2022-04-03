aab8e433635c1f4878561325ef99cd5908431e2d
divya <divya.aswale7@gmail.com>
2022-04-03 17:23:19 +0530

    commit

---
+++ 3_Implementation/inc/operations.h
@@ -0,0 +1,37 @@
+/**
+ * @file operations.h
+ * @author Divya (divya.aswale7@gmail.com)
+ * @brief clothinh store management
+ * @version 0.1
+ * 
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+//Include files
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<time.h>
+#include<add.c>
+#include<edit.c>
+
+
+// function declarations  
+
+void wel_come(void);
+void title(void);
+void login();
+void menu(void);
+void title(void);
+
+void add_item();
+void read_item();
+void search_item();
+void edit_item();
+void total_bill();
